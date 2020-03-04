927ad07036ba5871de2fa220ac6ba39f6ea7cc7d
Hani Husamuddin <hani.husam@gmail.com>
2020-03-05 06:04:43 +0700

    first commit

---
+++ polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+
+class ChoiceInliner(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ['question_text']}),
+        ("Date Information", {"fields": [
+         'pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInliner]
+
+
+admin.site.register(Question, QuestionAdmin)
